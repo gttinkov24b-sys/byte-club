2335b0a71c902693fb5c932a1787753e42fe718a
Griwo <g.t.tinkov.24b@gmail.com>
2025-11-11 19:08:27 +0200

    11.11

--- 11.11/zad8.py
+++ 11.11/zad8.py
@@ -1,5 +1,4 @@
 import turtle
-from types import BuiltinFunctionType
 t = turtle.Turtle()
 t.width(3)
 t.speed(5)
@@ -7,7 +6,7 @@ t.speed(5)
 t.fillcolor("blue")
 t.begin_fill()
 for i in range(5):
-    t.forward(115)
+    t.forward(225)
     t.right(144)
 t.end_fill()
 t.right(36)
